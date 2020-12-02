1fd37938dd369b5f7bfd8ef55ea6df9c66c0cbc9
Sander Nordby Larsen <sanderlarsen@Sanders-MBP-2.lan>
2020-12-02 23:31:39 +0100

    Tema 2.

--- JavaScript/butikk.js
+++ JavaScript/butikk.js
@@ -1,9 +1,15 @@
+
+//Koden for å sjekke om dokumentet har lastet inn (loaded) før man prøver
+//å bruke deler av det, viktig å ha i alle js man jobber med
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
     ready()
 }
 
+//Her legger vi til en knapp som gjør at man kan fjerne varer fra handlekurv. 
+//Button-elementet kommuniserer med remove-elementet, ved hjelp av addEventListener
+//og et click-event gjør at det blir mulig å fjerne varen.
 function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
     for (var i = 0; i < removeCartItemButtons.length; i++) {
@@ -11,21 +17,26 @@ function ready() {
         button.addEventListener('click', removeCartItem)
     }
 
+//'Change' gjør at vi kan justere antallet eller the quantity.
+//"Endre kvantitet i handlekurven"
     var quantityInputs = document.getElementsByClassName('cart-quantity-input')
     for (var i = 0; i < quantityInputs.length; i++) {
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
 
+//Legge til varer i handlekurven ved å 'click" på en button
     var addToCartButtons = document.getElementsByClassName('shop-item-button')
     for (var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
 
+//Betale-knapp
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
 
+//Beskjed om at varene er kjøpt, en pop-up
 function purchaseClicked() {
     alert('Takk for at du handlet hos oss!')
     var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -35,12 +46,14 @@ function purchaseClicked() {
     updateCartTotal()
 }
 
+//Fjerne vare fra handlekurv 
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
 }
 
+//Endre antall av en vare i handlekurv
 function quantityChanged(event) {
     var input = event.target
     if (isNaN(input.value) || input.value <= 0) {
@@ -49,6 +62,7 @@ function quantityChanged(event) {
     updateCartTotal()
 }
 
+//Gjør at både tittel, pris og bilde følger med til en oversikt over varer i handlekurven
 function addToCartClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
@@ -59,6 +73,10 @@ function addToCartClicked(event) {
     updateCartTotal()
 }
 
+//Legger inn en sperre på varer som allerede er i handlekurv, 
+//med alert (for å varsle at den allerede er lagt til) og
+//for å ikke bruke html-koden som fortsetter videre i koden så
+//legges det inn en return som stopper den fra å bruke den delen av koden
 function addItemToCart(title, price, imageSrc) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
@@ -70,6 +88,7 @@ function addItemToCart(title, price, imageSrc) {
             return
         }
     }
+    //Bruker html-koden direkte for å lage en cart-row (oversikt) i handlekurven
     var cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
@@ -86,6 +105,8 @@ function addItemToCart(title, price, imageSrc) {
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
+//Oppdaterer handlekurven med samlet sum for de valgte varene i (kr)
+//samt at illutrasjonen av varen legges i kurven med et valg om å endre antallet man ønsker å kjøpe
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
